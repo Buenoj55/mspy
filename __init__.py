62a48f91a791cc314198fc00194a31b19fdf8974
Buenoj55 <jerome.bueno@hotmail.fr>
2018-04-12 15:39:40 +0200

    Update and rename mLite.py to __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,13 @@
 from flask import Flask,request, render_template
+from flask import flash
 from key import Key
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/', methods=['POST'])
-def createRoom():
-    roomKey = request.form['initial']+generateRootKey()
+    roomKey = generateRootKey()
     return render_template('index.html',key=roomKey)
 
 def generateRootKey():
@@ -17,9 +15,17 @@ def generateRootKey():
     chatPin = key.getKey()
     return chatPin
 
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/<key>/')
 def chat(key):
-    return render_template('room.html', key=key)
+    flash(u'Bienvenue dans votre chat')
+    return render_template('room.html',key=key)
 
 if __name__ == '__main__':
+    app.secret_key = 'A7§/23/3KF093250'
     app.run(debug=True)
+
+

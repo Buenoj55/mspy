6e8ad26bac09850b57cf3de2a4f85cb568965de7
Buenoj55 <jerome.bueno@hotmail.fr>
2018-04-16 16:48:55 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,31 +1,47 @@
-from flask import Flask,request, render_template
+from flask import Flask,request, render_template, session, url_for,redirect
 from flask import flash
 from key import Key
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    roomKey = generateRootKey()
-    return render_template('index.html',key=roomKey)
+    generate = request.form.get('generate', 'False')
+    
+    if(generate == 'Generer un chat'):
+        if('roomKey' in session):
+            return redirect(url_for('room',key=session['roomKey']))
+        else:
+            initial = request.form.get('initial')
+            session['roomKey'] = initial.upper() +  generateRouteKey()
+            return redirect(url_for('room',key=session['roomKey']))
+    else:
+        return render_template('index.html')
 
-def generateRootKey():
+def generateRouteKey():
     key = Key()
-    chatPin = key.getKey()
-    return chatPin
+    RouteKey = key.getKey()
+    return RouteKey
 
-@app.route('/contact/')
+
+@app.route('/contact/',methods=['GET','POST'])
 def contact():
+    if request.method == 'POST':
+        return "Vous avez envoyé : {msg}".format(msg=request.form.get('msg', 'valeur par défaut'))
     return render_template('contact.html')
 
+
 @app.route('/<key>/')
-def chat(key):
-    flash(u'Bienvenue dans votre chat')
+def room(key):
     return render_template('room.html',key=key)
+
+
+@app.route('/test/')
+def test():
+    return 'a'
+
 
 if __name__ == '__main__':
     app.secret_key = 'A7§/23/3KF093250'
     app.run(debug=True)
-
-
